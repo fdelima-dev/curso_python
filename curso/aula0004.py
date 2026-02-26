eb8e04684c3f84de2eeffa7a7d31ade3e1793322
fdelima-dev <fdelima.dev@gmail.com>
2026-02-26 07:06:02 -0300

    adicionando arquivos

---
+++ curso/aula0004.py
@@ -0,0 +1,23 @@
+#tipos int e float
+#int -> Número inteiro
+#O tipo int representa qualquer número
+#positivo ou negativo. Int sem sinal é considerado
+#positivo
+
+print(11)
+print(-11)
+print(0)
+
+#Float -> Número com ponto flutuante
+#o tipo float representa qualquer número
+#positivo ou negativo com ponto flutuante
+#float sem sinal é considerado positivo
+
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+#A função type mostra o tipo que o Python
+#inferiu ao valor
+print(type('Felipe'))
+print(type(1))
+print(type(1.1), type(-1.1), type(0.0))
---
+++ curso/aula0005.py
@@ -0,0 +1,14 @@
+#tipo de dado bool (booleano)
+#ao questionar algo em um programa
+#Só existem duas respostas possível
+#Sim (True) Não (False)
+#Existe operadores para "questionar".
+#Dentre eles o == que é um operador lógico que
+#Questiona se um valor é igual a outro
+
+print(10 == 10)
+print(10 == 11)
+print(type(10 == 10))
+print(type(10 == 11))
+print(type(True))
+print(type(False))
