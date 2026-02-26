9d059da1aa577db6a6786b0e2750e8b85cb4a714
fdelima-dev <fdelima.dev@gmail.com>
2026-02-26 07:42:04 -0300

    Novas aulas

---
+++ curso/aula0007.py
@@ -0,0 +1,14 @@
+#variaveis são usadas pra salvar algo na
+#memoria do computador
+#PEP8: inicie variáveis com letras minúsculas, pode 
+#usar números e underline _
+#O sinal de = é o operador de atribuição.
+#Ele é usado para atribuir valor a uma variável
+#Uso: nome_variavel = expressao 
+#snake_case
+
+nome = 'Felipe'
+idade = 33
+maior_de_idade = idade >= 18
+print('Nome:', nome, "Idade:", idade)
+print('É maior de idade?', maior_de_idade)
---
+++ curso/aula0008.py
@@ -0,0 +1,13 @@
+nome = 'Felipe'
+sobrenome = 'de Lima Lara'
+idade = 33
+ano_nasc = 2026 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.75
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano Nascimento: ", ano_nasc)
+print("Maior de idade: ", maior_de_idade)
+print("Altura em metros: ", altura_metros)
