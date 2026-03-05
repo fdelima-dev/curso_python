186a3f89a54719d6df9701c434a18a6a3d294b92
Felipe de Lima Lara <159076031+fdelima-dev@users.noreply.github.com>
2026-03-05 15:40:49 -0300

    Delete curso directory

--- curso/aula0012.py
+++
@@ -1,7 +0,0 @@
-nome = 'Felipe Lima'
-peso = 110
-altura = 1.75
-
-imc = peso / (altura * altura)
-
-print(nome, 'tem', peso, 'quilos e tem', altura,'m de altura e seu imc é', imc)
--- curso/aula0014.py
+++
@@ -1,8 +0,0 @@
-a = 'A'
-b = 'B'
-c = 1.1
-
-string = 'a= {} b= {} c= {:.2f}'
-formato = string.format(a, b, c)
-
-print(formato)
